d23b81daf4d7c46524da27870cde453dfcda56d7
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-10-22 15:43:45 -0300

    chore: add auth migrate and nextauth setup

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Card } from "./card";
 import { Button } from "./button";
-import { MenuIcon } from "lucide-react";
+import { HomeIcon, MenuIcon, ShellIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 export const Header = () => {
   return (
-    <Card className="flex p-[1.875rem] justify-between items-center">
+    <Card className="flex items-center justify-between gap-3 p-[1.875rem]">
       <Link href={"/"}>
         <Image
           src={"/logo.png"}
@@ -18,13 +19,45 @@ export const Header = () => {
             objectFit: "contain",
           }}
           alt="Waves Sustentabilidades"
-          className="w-[150px] h-full"
+          className="h-full w-[150px]"
         />
       </Link>
 
-      <Button size="icon" variant="outline">
-        <MenuIcon />
-      </Button>
+      <div className="flex gap-4 items-center justify-center">
+        <Button className="font-semibold">Cadastrar</Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent>
+            <SheetHeader className="text-left text-lg font-semibold">
+              Menu
+            </SheetHeader>
+
+            <div className="mt-4 flex flex-col gap-2">
+              <Button
+                variant="outline"
+                className="flex w-full justify-start gap-2 "
+              >
+                <HomeIcon size={16} />
+                Início
+              </Button>
+
+              <Button
+                variant="outline"
+                className="flex w-full justify-start gap-2 "
+              >
+                <ShellIcon size={16} />
+                Sobre
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
     </Card>
   );
 };
