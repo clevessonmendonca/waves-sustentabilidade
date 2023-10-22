ada9150362cc7203f1bd24b3d3349d9b3ad12fe6
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-10-22 16:31:29 -0300

    add auth handler header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+"use client";
+
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  return (
-    <h1>Hellop</h1>
-  )
+  const { data } = useSession();
+
+  return <div>{data?.user?.name}</div>
 }
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { Card } from "./card";
 import { Button } from "./button";
-import { HomeIcon, MenuIcon, ShellIcon } from "lucide-react";
+import { HomeIcon, LogOutIcon, MenuIcon, ShellIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
+import { useRouter } from "next/navigation";
 
 export const Header = () => {
+  const router = useRouter();
+  const { status, data } = useSession();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      return router.push("/");
+    }
+  }, [router, status]);
+
+  const handleLoginClick = async () => {
+    await signIn("google", { callbackUrl: "/dashboard" });
+  };
+
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
+
   return (
     <Card className="flex items-center justify-between gap-3 p-[1.875rem]">
       <Link href={"/"}>
@@ -23,8 +46,12 @@ export const Header = () => {
         />
       </Link>
 
-      <div className="flex gap-4 items-center justify-center">
-        <Button className="font-semibold">Cadastrar</Button>
+      <div className="flex items-center justify-center gap-4">
+        {status === "unauthenticated" && (
+          <Button className="font-semibold" onClick={handleLoginClick}>
+            Cadastrar
+          </Button>
+        )}
 
         <Sheet>
           <SheetTrigger asChild>
@@ -38,22 +65,60 @@ export const Header = () => {
               Menu
             </SheetHeader>
 
-            <div className="mt-4 flex flex-col gap-2">
-              <Button
-                variant="outline"
-                className="flex w-full justify-start gap-2 "
-              >
-                <HomeIcon size={16} />
-                Início
-              </Button>
+            <div className="mt-4 flex h-full flex-col justify-between gap-2 pb-16">
+              <div className="flex flex-col gap-2">
+                <Button
+                  variant="outline"
+                  className="flex w-full justify-start gap-2 "
+                >
+                  <HomeIcon size={16} />
+                  Início
+                </Button>
 
-              <Button
-                variant="outline"
-                className="flex w-full justify-start gap-2 "
-              >
-                <ShellIcon size={16} />
-                Sobre
-              </Button>
+                <Button
+                  variant="outline"
+                  className="flex w-full justify-start gap-2 "
+                >
+                  <ShellIcon size={16} />
+                  Sobre
+                </Button>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                {status === "authenticated" && data?.user && (
+                  <div className="flex flex-col">
+                    <div className="flex items-center gap-2 py-4">
+                      <Avatar>
+                        <AvatarFallback>
+                          {data.user.name?.[0].toUpperCase()}
+                        </AvatarFallback>
+
+                        {data.user.image && (
+                          <AvatarImage src={data.user.image} />
+                        )}
+                      </Avatar>
+
+                      <div className="flex flex-col">
+                        <p className="font-medium">{data.user.name}</p>
+                        <p className="text-sm opacity-75">Reciclador</p>
+                      </div>
+                    </div>
+
+                    <Separator />
+                  </div>
+                )}
+
+                {status === "authenticated" && (
+                  <Button
+                    className="gap-2 font-semibold "
+                    variant="outline"
+                    onClick={handleLogoutClick}
+                  >
+                    <LogOutIcon size={16} />
+                    Deslogar
+                  </Button>
+                )}
+              </div>
             </div>
           </SheetContent>
         </Sheet>
