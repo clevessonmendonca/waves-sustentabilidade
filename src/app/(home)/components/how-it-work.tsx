542f09f8d94989f9fb2826c07336918721575dff
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-11-02 11:35:17 -0300

    feat: Ladign page HowItWork Component

---
+++ src/app/(home)/components/how-it-work.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import Image from "next/image";
+
+export const HowItWork = () => {
+  return (
+    <div className="relative mt-52">
+      <h2 className="text-center text-2xl font-bold text-primary">
+        Como funcionamos
+      </h2>
+
+      <span className="top-4/4 absolute -z-10 w-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1418"
+          height="513"
+          viewBox="0 0 1418 513"
+          fill="none"
+          className=" -z-10 w-full"
+        >
+          <path
+            d="M0 513.533C0 513.533 21.0883 338.692 63.8889 338.692C91.3972 338.692 105.394 427.439 112.693 453.592C127.261 505.783 189.004 587.47 300.809 385.654C425.591 160.408 617.176 131.767 794.169 228.792C963.663 321.705 1220.12 315.721 1330.12 185.831C1428.22 70.007 1418.86 0 1418.86 0"
+            stroke="#00B4FC"
+            stroke-opacity="0.09"
+            stroke-width="3"
+          />
+        </svg>
+      </span>
+      <div className="mt-8 flex flex-col items-center justify-center gap-16">
+        <div className="flex flex-col items-center justify-center gap-8 px-5 md:flex-row">
+          <div className="mt-12 flex max-w-sm flex-col gap-2">
+            <span className="text-xs text-primary">Passo 1</span>
+            <h3 className="text-lg font-bold">Coleta Consciente</h3>
+            <p className="text-sm opacity-75 ">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla,
+              debitis eum natus ullam at temporibus quo aspernatur quasi earum
+              distinctio perferendis expedita dolores quisquam aut ipsum cum
+              neque sint. Placeat.
+            </p>
+          </div>
+
+          <Image
+            src={"/banner-home.webp"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{
+              objectFit: "contain",
+            }}
+            alt="Waves Sustentabilidades"
+            className="mt-4 h-full max-h-[200px] w-full"
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-8 px-5 md:flex-row">
+          <div className="mt-12 flex max-w-sm flex-col gap-2 md:order-2">
+            <span className="text-xs text-primary">Passo 1</span>
+            <h3 className="text-lg font-bold">Coleta Consciente</h3>
+            <p className="text-sm opacity-75 ">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla,
+              debitis eum natus ullam at temporibus quo aspernatur quasi earum
+              distinctio perferendis expedita dolores quisquam aut ipsum cum
+              neque sint. Placeat.
+            </p>
+          </div>
+
+          <Image
+            src={"/banner-home.webp"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{
+              objectFit: "contain",
+            }}
+            alt="Waves Sustentabilidades"
+            className="mt-4 h-full max-h-[200px] w-full md:order-1"
+          />
+        </div>
+
+        <div className="flex flex-col items-center justify-center gap-8 px-5 md:flex-row">
+          <div className="mt-12 flex max-w-sm flex-col gap-2">
+            <span className="text-xs text-primary">Passo 1</span>
+            <h3 className="text-lg font-bold">Coleta Consciente</h3>
+            <p className="text-sm opacity-75 ">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla,
+              debitis eum natus ullam at temporibus quo aspernatur quasi earum
+              distinctio perferendis expedita dolores quisquam aut ipsum cum
+              neque sint. Placeat.
+            </p>
+          </div>
+
+          <Image
+            src={"/banner-home.webp"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{
+              objectFit: "contain",
+            }}
+            alt="Waves Sustentabilidades"
+            className="mt-4 h-full max-h-[200px] w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,35 +1,55 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { HowItWork } from "./components/how-it-work";
+import { ArrowRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
-      <Image
-        src={"/banner-home.webp"}
-        width={0}
-        height={0}
-        sizes="100vw"
-        style={{
-          objectFit: "contain",
-        }}
-        alt="Waves Sustentabilidades"
-        className="absolute top-0 -z-10 h-auto w-full max-w-screen-2xl opacity-60"
-      />
+      <div className="relative">
+        <div>
+          <Image
+            src={"/banner-home.webp"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{
+              objectFit: "cover",
+            }}
+            alt="Waves Sustentabilidades"
+            className="absolute -top-40 -z-20 h-screen max-h-[80vh] w-full max-w-screen-2xl opacity-60"
+          />
 
-      <div className="mt-8 flex flex-col items-center justify-center gap-2">
-        <h1 className="max-w-md px-5 text-center text-3xl font-bold">
-          Salvando Nossos Oceanos! <span className="text-primary">Juntos</span>{" "}
-          Pela Reciclagem Marinha
-        </h1>
-        <p className="max-w-sm text-center text-xs opacity-75">
-          Unindo Forças para Limpar e Proteger os Tesouros Subaquáticos do Nosso
-          Planeta
-        </p>
-
-        <div className="mt-5 flex gap-4">
-          <Button>Sou um Coletador</Button>
-          <Button>Sou um Reciclador</Button>
+          <span className="absolute -z-10 h-screen max-h-[80vh] w-full max-w-screen-2xl bg-gradient-to-b from-transparent from-30% via-background to-background" />
         </div>
+
+        <div className="mt-12 flex flex-col items-center justify-center gap-2 px-5">
+          <h1 className="max-w-md px-5 text-center text-3xl font-bold drop-shadow-md">
+            Seja uma <span className="text-primary">Ponte</span> para salvar os
+            Oceanos!
+          </h1>
+          <p className="max-w-sm text-center text-xs opacity-75">
+            Unindo Forças para Limpar e Proteger os Tesouros Subaquáticos do
+            Nosso Planeta
+          </p>
+
+          <div className="mt-5 flex gap-4">
+            <Button className="shadow-lg">Salve o oceano</Button>
+            <Button
+              variant="outline"
+              className="flex gap-2 border-none bg-transparent shadow-md"
+            >
+              Saiba mais
+              <ArrowRightIcon size={12} />
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <HowItWork />
+
+      <div>
+        <div></div>
       </div>
     </div>
   );
