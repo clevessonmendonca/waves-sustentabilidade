c72b1c135ad8544ff9a7a14cd0d7a6cfcccb96bf
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-11-21 03:45:20 -0300

    fix: request sendgrid

--- src/app/api/sendgrid/route.ts
+++ src/app/api/sendgrid/route.ts
@@ -1,25 +1,25 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { sendEmail } from "@/lib/email";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { sendEmail } from "@/lib/email";
 
-interface RequestWithBody extends NextApiRequest {
-  body: {
-    name: string;
-    email: string;
-    message: string;
-  };
-}
+// interface RequestWithBody extends NextApiRequest {
+//   body: {
+//     name: string;
+//     email: string;
+//     message: string;
+//   };
+// }
 
-export async function POST(req: RequestWithBody, res: NextApiResponse) {
-  if (req.method === "POST") {
-    try {
-      const { name, email, message } = req.body;
+// export async function POST(req: RequestWithBody, res: NextApiResponse) {
+//   if (req.method === "POST") {
+//     try {
+//       const { name, email, message } = req.body;
 
-      await sendEmail(name, email, message);
-      res.status(200).json({ message: "Email enviado com sucesso" });
-    } catch (error) {
-      res.status(500).json({ message: "Erro ao enviar o email" });
-    }
-  } else {
-    res.status(405).json({ message: "Método não permitido" });
-  }
-}
+//       await sendEmail(name, email, message);
+//       res.status(200).json({ message: "Email enviado com sucesso" });
+//     } catch (error) {
+//       res.status(500).json({ message: "Erro ao enviar o email" });
+//     }
+//   } else {
+//     res.status(405).json({ message: "Método não permitido" });
+//   }
+// }
