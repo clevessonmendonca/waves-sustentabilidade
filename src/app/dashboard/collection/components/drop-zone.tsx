59a65b1b0123e7657bbe8e8675cf4c1405fcf9a4
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-11-23 23:10:27 -0300

    test: test to deploy

--- src/app/dashboard/collection/components/drop-zone.tsx
+++ src/app/dashboard/collection/components/drop-zone.tsx
@@ -1,72 +1,45 @@
 import { Card } from "@/components/ui/card";
-import { toast } from "@/components/ui/use-toast";
 import { FolderDownIcon } from "lucide-react";
 import Image from "next/image";
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 
-interface DropzoneProps {
-  onFileUpload: (files: File[]) => void;
-}
-
-export const Dropzone = ({ onFileUpload }: DropzoneProps) => {
+export const Dropzone = () => {
   const [files, setFiles] = useState<any>([]);
-  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
-    maxFiles: 2,
-    accept: {
-      "image/png": [".png"],
-      "text/html": [".html", ".htm"],
-    },
-    onDrop: (acceptedFiles) => {
-      const acceptedFile = acceptedFiles[0];
-
-      if (!acceptedFile) {
-        toast({
-          title: "Formato Inválido!",
-          description:
-            "Por favor, selecione uma imagem válida (PNG, JPG ou WebP)",
-          variant: "destructive",
-        });
-        return;
-      }
-
-      setFiles(
-        acceptedFiles.map((file) =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file),
-          })
-        )
-      );
-    },
-  });
+  const { getRootProps, getInputProps, acceptedFiles, fileRejections } =
+    useDropzone({
+      maxFiles: 2,
+      accept: {
+        "image/png": [".png"],
+        "text/html": [".html", ".htm"],
+      },
+      onDrop: (acceptedFiles) => {
+        setFiles(
+          acceptedFiles.map((file) =>
+            Object.assign(file, {
+              preview: URL.createObjectURL(file),
+            }),
+          ),
+        );
+      },
+    });
 
   const Preview = files.map((file: any) => (
     <div key={file.name}>
-      <Image
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="h-full max-h-40 w-full"
-        src={file.preview}
-        title={file.name}
-        alt={file.name}
-      />
+      <Image width={0} height={0} sizes="100vw" className="max-h-40 w-full h-full" src={file.preview} title={file.name} alt={file.name} />
     </div>
   ));
 
   return (
-    <Card
-      {...getRootProps({ className: "flex h-48 w-full cursor-pointer items-center justify-center border-2 border-dashed border-gray-300" })}
-    >
-      <input {...getInputProps()} />
-      {acceptedFiles.length === 0 ? (
-        <p className="flex flex-col items-center gap-4 text-sm text-gray-600 opacity-75">
+    <Card className="flex h-48 w-full cursor-pointer items-center justify-center border-2 border-dashed border-gray-300">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p className="text-sm text-gray-600 opacity-75 flex gap-4 items-center flex-col">
           <FolderDownIcon size={32} className="text-gray-400" />
           Drop some files here ...
         </p>
-      ) : (
+      </div>
         <div>{Preview}</div>
-      )}
     </Card>
   );
 };
