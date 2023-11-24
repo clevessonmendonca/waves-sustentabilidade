7e277ed0021c1894a709447a092e82c5bde4ab6c
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-11-23 23:25:31 -0300

    fix: errors

--- src/app/dashboard/collection/components/drop-zone.tsx
+++ src/app/dashboard/collection/components/drop-zone.tsx
@@ -1,8 +1,7 @@
-import { Card } from "@/components/ui/card";
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 
-export const Dropzone = () => {
+export const Dropzone: React.FC = () => {
   const [files, setFiles] = useState<any>([]);
   const { getRootProps, getInputProps, acceptedFiles, fileRejections } =
     useDropzone({
@@ -16,33 +15,20 @@ export const Dropzone = () => {
           acceptedFiles.map((file) =>
             Object.assign(file, {
               preview: URL.createObjectURL(file),
-            }),
-          ),
+            })
+          )
         );
       },
     });
 
-  const Preview = files.map((file: any) => (
-    <div key={file.name}>
-      <iframe src={file.preview} title={file.name} />
-    </div>
-  ));
-
-  const acceptedFileItems = acceptedFiles.map((file) => (
-    <li key={file.name}>{file.name}</li>
-  ));
-  const fileRejectionItems = fileRejections.map(({ file, errors }) => (
-    <li key={file.name}>{file.name}</li>
-  ));
-
   return (
-    <Card className="flex h-48 w-full cursor-pointer items-center justify-center border-2 border-dashed border-gray-300">
+    <div className="container">
       <div {...getRootProps({ className: "dropzone" })}>
         <input {...getInputProps()} />
         <p>Drop some files here ...</p>
       </div>
-
-      <div>{Preview}</div>
-    </Card>
+      <div>
+      </div>
+    </div>
   );
 };
