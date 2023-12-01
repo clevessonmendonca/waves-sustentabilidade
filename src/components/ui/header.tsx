356e6d0fda38de60bc1eee29f56ee9810ffdac3a
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-11-30 23:20:39 -0300

    fix: error in loading dashboard

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -36,6 +36,8 @@ import { Separator } from "@/components/ui/separator";
 import { getNotification } from "@/app/actions/getNotifications";
 import { Notification } from "@prisma/client";
 import { NotificationList } from "./notification-list";
+import { Dialog, DialogHeader, DialogContent } from "./dialog";
+import { DialogDescription } from "@radix-ui/react-dialog";
 
 export const handleLoginClick = async () => {
   await signIn("google", { callbackUrl: "/dashboard" });
@@ -67,163 +69,165 @@ export const Header = () => {
   }, [router, status]);
 
   return (
-    <Card className="mx-auto flex w-full max-w-screen-xl items-center justify-between gap-3 border-none bg-transparent p-[1.875rem] shadow-none">
-      <Link href={"/"}>
-        <Image
-          src={"/logo.png"}
-          width={0}
-          height={0}
-          sizes="100vw"
-          style={{
-            objectFit: "contain",
-          }}
-          alt="Waves Sustentabilidades"
-          className="h-full w-[150px]"
-        />
-      </Link>
+    <>
+      <Card className="mx-auto flex w-full max-w-screen-xl items-center justify-between gap-3 border-none bg-transparent p-[1.875rem] shadow-none">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.png"}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{
+              objectFit: "contain",
+            }}
+            alt="Waves Sustentabilidades"
+            className="h-full w-[150px]"
+          />
+        </Link>
 
-      <div className="flex items-center justify-center gap-4">
-        {status === "unauthenticated" ? (
-          <Button className="font-semibold" onClick={handleLoginClick}>
-            Cadastrar
-          </Button>
-        ) : (
-          <div className="flex gap-5">
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <BellDotIcon />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel className="flex items-center justify-between gap-16 px-2">
-                  Notificações{" "}
-                  <Button className="px-0" variant="link">
-                    Marcar todas como lidas.
-                  </Button>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <div className="flex flex-col gap-1 px-0 py-2">
-                  <NotificationList />
-                </div>
-              </DropdownMenuContent>
-            </DropdownMenu>
+        <div className="flex items-center justify-center gap-4">
+          {status === "unauthenticated" ? (
+            <Button className="font-semibold" onClick={handleLoginClick}>
+              Cadastrar
+            </Button>
+          ) : (
+            <div className="flex gap-5">
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <BellDotIcon />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel className="flex items-center justify-between gap-16 px-2">
+                    Notificações{" "}
+                    <Button className="px-0" variant="link">
+                      Marcar todas como lidas.
+                    </Button>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <div className="flex flex-col gap-1 px-0 py-2">
+                    <NotificationList />
+                  </div>
+                </DropdownMenuContent>
+              </DropdownMenu>
 
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-                <Avatar>
-                  <AvatarFallback>
-                    {data?.user.name?.[0].toUpperCase()}
-                  </AvatarFallback>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar>
+                    <AvatarFallback>
+                      {data?.user.name?.[0].toUpperCase()}
+                    </AvatarFallback>
 
-                  {data?.user.image && <AvatarImage src={data.user.image} />}
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <Link
-                    href="dashboard/profile"
+                    {data?.user.image && <AvatarImage src={data.user.image} />}
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem>
+                    <Link
+                      href="dashboard/profile"
+                      className="flex items-center gap-2"
+                    >
+                      <User2Icon /> Perfil
+                    </Link>
+                  </DropdownMenuItem>
+
+                  <DropdownMenuSeparator className="mt-2" />
+
+                  <DropdownMenuItem
+                    onClick={handleLogoutClick}
                     className="flex items-center gap-2"
                   >
-                    <User2Icon /> Perfil
-                  </Link>
-                </DropdownMenuItem>
-
-                <DropdownMenuSeparator className="mt-2" />
-
-                <DropdownMenuItem
-                  onClick={handleLogoutClick}
-                  className="flex items-center gap-2"
-                >
-                  <LogOutIcon /> Sair
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        )}
-
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              size="icon"
-              variant="outline"
-              className="border-none bg-transparent drop-shadow-md"
-            >
-              <MenuIcon />
-            </Button>
-          </SheetTrigger>
-
-          <SheetContent>
-            <SheetHeader className="text-left text-lg font-semibold">
-              Menu
-            </SheetHeader>
-
-            <div className="mt-4 flex h-full flex-col justify-between gap-2 pb-16">
-              <div className="flex flex-col gap-2">
-                <Button
-                  variant="outline"
-                  className="flex w-full justify-start gap-2 "
-                >
-                  <HomeIcon size={16} />
-                  Início
-                </Button>
-
-                <Button
-                  variant="outline"
-                  className="flex w-full justify-start gap-2 "
-                >
-                  <ShellIcon size={16} />
-                  Sobre
-                </Button>
-
-                <Separator />
-
-                <Link href="/signin/collector">
-                  <Button className="mt-2 flex w-full justify-center gap-2 ">
-                    <RecycleIcon size={16} />
-                    Torna-se Coletador
-                  </Button>
-                </Link>
-              </div>
-
-              <div className="flex flex-col gap-2">
-                {status === "authenticated" && data?.user && (
-                  <div className="flex flex-col">
-                    <div className="flex items-center gap-2 py-4">
-                      <Avatar>
-                        <AvatarFallback>
-                          {data.user.name?.[0].toUpperCase()}
-                        </AvatarFallback>
-
-                        {data.user.image && (
-                          <AvatarImage src={data.user.image} />
-                        )}
-                      </Avatar>
-
-                      <div className="flex flex-col">
-                        <p className="font-medium">{data.user.name}</p>
-                      </div>
-                    </div>
-
-                    <Separator />
-                  </div>
-                )}
-
-                {status === "authenticated" && (
-                  <Button
-                    className="gap-2 font-semibold "
-                    variant="outline"
-                    onClick={handleLogoutClick}
-                  >
-                    <LogOutIcon size={16} />
-                    Deslogar
-                  </Button>
-                )}
-              </div>
+                    <LogOutIcon /> Sair
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </div>
-          </SheetContent>
-        </Sheet>
-      </div>
-    </Card>
+          )}
+
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                size="icon"
+                variant="outline"
+                className="border-none bg-transparent drop-shadow-md"
+              >
+                <MenuIcon />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent>
+              <SheetHeader className="text-left text-lg font-semibold">
+                Menu
+              </SheetHeader>
+
+              <div className="mt-4 flex h-full flex-col justify-between gap-2 pb-16">
+                <div className="flex flex-col gap-2">
+                  <Button
+                    variant="outline"
+                    className="flex w-full justify-start gap-2 "
+                  >
+                    <HomeIcon size={16} />
+                    Início
+                  </Button>
+
+                  <Button
+                    variant="outline"
+                    className="flex w-full justify-start gap-2 "
+                  >
+                    <ShellIcon size={16} />
+                    Sobre
+                  </Button>
+
+                  <Separator />
+
+                  <Link href="/signin/collector">
+                    <Button className="mt-2 flex w-full justify-center gap-2 ">
+                      <RecycleIcon size={16} />
+                      Torna-se Coletador
+                    </Button>
+                  </Link>
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  {status === "authenticated" && data?.user && (
+                    <div className="flex flex-col">
+                      <div className="flex items-center gap-2 py-4">
+                        <Avatar>
+                          <AvatarFallback>
+                            {data.user.name?.[0].toUpperCase()}
+                          </AvatarFallback>
+
+                          {data.user.image && (
+                            <AvatarImage src={data.user.image} />
+                          )}
+                        </Avatar>
+
+                        <div className="flex flex-col">
+                          <p className="font-medium">{data.user.name}</p>
+                        </div>
+                      </div>
+
+                      <Separator />
+                    </div>
+                  )}
+
+                  {status === "authenticated" && (
+                    <Button
+                      className="gap-2 font-semibold "
+                      variant="outline"
+                      onClick={handleLogoutClick}
+                    >
+                      <LogOutIcon size={16} />
+                      Deslogar
+                    </Button>
+                  )}
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </Card>
+    </>
   );
 };
